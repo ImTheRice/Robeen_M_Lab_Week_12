ecbec38176dff5bc0f91cf96ef03610938cb8a78
Robeen M <maharjanrobeen@gmail.com>
2022-03-23 23:21:11 -0700

    Deleted unnecessary repeat of students in app.js

--- src/app.js
+++ src/app.js
@@ -5,12 +5,6 @@ function selector() {
 
 console.log(selector())
 
-const students = [
-    { name: "Tasha", grades: [80, 67, 75, 62] },
-    { name: "Tuvok", grades: [90, 88, 91, 92] },
-    { name: "Dax", grades: [88, 90, 89, 85] },
-    { name: "Neelix", grades: [60, 54, 48, 50] }
-]
 
 function scholarfunction() {
 
