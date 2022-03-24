f377f4ed9c7a0c03d715c2968b3e690ef4b525a0
Robeen M <maharjanrobeen@gmail.com>
2022-03-23 23:18:09 -0700

    Part Three Completed

--- src/app.js
+++ src/app.js
@@ -4,3 +4,41 @@ function selector() {
 }
 
 console.log(selector())
+
+const students = [
+    { name: "Tasha", grades: [80, 67, 75, 62] },
+    { name: "Tuvok", grades: [90, 88, 91, 92] },
+    { name: "Dax", grades: [88, 90, 89, 85] },
+    { name: "Neelix", grades: [60, 54, 48, 50] }
+]
+
+function scholarfunction() {
+
+    const students = [
+        { name: "Tasha", grades: [80, 67, 75, 62] },
+        { name: "Tuvok", grades: [90, 88, 91, 92] },
+        { name: "Dax", grades: [88, 90, 89, 85] },
+        { name: "Neelix", grades: [60, 54, 48, 50] }
+    ]
+
+    const studentsScholarship = []
+
+    let sum = 0
+
+    for (let student of students) {
+        for (let grade of student.grades) {
+            sum += grade
+        }
+        if (sum / student.grades.length > 80) {
+            student['gradeAvg'] = student['grades']
+            student.gradeAvg = (sum / student.grades.length)
+            delete student['grades']
+            studentsScholarship.push(student)
+        }
+        sum = 0
+    }
+
+    return studentsScholarship
+
+}
+console.log(scholarfunction())
